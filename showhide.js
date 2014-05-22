779cb56d6265b4be2170d09d6debe622b61acde6
Andrew Kennedy <akennedy2292@gmail.com>
2014-05-22 15:06:53 +0000

    support elements other than anchor tags

--- showhide.js
+++ showhide.js
@@ -1,31 +1,44 @@
 (function($){ 
   $(document).ready(function() {
-    var elems = $('*[showhide]');
-    for (var i = 0; i < elems.length; i++) {
-      var $elem = elems.eq(i);
-      var settings = JSON.parse($elem.text());
-      var id = "#" + $elem.attr("showhide");
-      $elem.data("wpsh_show", settings[0]);
-      $elem.data("wpsh_hide", settings[1]);
-      $elem.data("wpsh_id", id);
-      if (settings.length > 2 && settings[2]) {
-        $elem.text(settings[1]);
-        $(id).hide();
-      }
-      else {
-        $elem.text(settings[0]);
-        $(id).show();
-      }
+    var elems = $('*[showhide]'),
+      $elem, id, text, settings;
 
-      $elem.click(function(e) {
-        var $elem = $(e.currentTarget);
-        var id = $elem.data("wpsh_id");
-        if ($(id).is(":visible")) {
-          $elem.text($elem.data("wpsh_hide"));
+    for (var i = 0; i < elems.length; i++) {
+      $elem = elems.eq(i);
+      id = "#" + $elem.attr("showhide");
+      text = $elem.text();
+
+      if (text){
+        settings = JSON.parse($elem.text());
+        $elem.data("wpsh_show", settings[0]);
+        $elem.data("wpsh_hide", settings[1]);
+
+        if (settings.length > 2 && settings[2]) {
+          $elem.text(settings[1]);
           $(id).hide();
         }
         else {
-          $elem.text($elem.data("wpsh_show"));
+          $elem.text(settings[0]);
+          $(id).show();
+        }
+      }
+
+      $elem.data("wpsh_id", id);
+
+      $elem.click(function(e) {
+        console.log(e);
+        var $elem = $(e.currentTarget);
+        var id = $elem.data("wpsh_id");
+        if ($(id).is(":visible")) {
+          if ($elem.data("wpsh_hide")){
+            $elem.text($elem.data("wpsh_hide"));
+          }
+          $(id).hide();
+        }
+        else {
+          if ($elem.data("wpsh_show")){
+            $elem.text($elem.data("wpsh_show"));
+          }
           $(id).show();
         }
         return false;
