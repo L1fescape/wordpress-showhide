f008dfba4ea5314c952ee244248b51ea67889b06
Andrew Kennedy <akennedy2292@gmail.com>
2014-05-24 19:36:12 +0000

    support showhide elements initially being visible. update readme with more examples.

--- README.md
+++ README.md
@@ -1,29 +1,45 @@
 # WordPress ShowHide
 
-> Show or hide what ever element you want with a mouse click.
+> Show or hide content with a mouse click.
 
 ## Demo
 View demo: http://akenn.org/projects/wp-showhide
 
-# Installation
+## Installation
 1. Unzip better-wordpress-showhide-elements.zip
 2. Upload better-wordpress-showhide-elements folder to the `/wp-content/plugins/` directory
 3. Activate the plugin through the 'Plugins' menu in WordPress
 
 ## Usage
 
+Example with image
+
+```html
+<img showhide='myId2' src='https://www.google.com/images/srpr/logo11w.png' />
+<div id='myId2'>Content goes here</div>
+```
+
 Example with anchor tag
 
-```
+```html
 <a showhide='myId1' href="#">
   ["When visible", "When hidden"]
 </a>
 <div id="myId1">Content goes here</div>
 ```
 
-Example with image
+Example with image with content initially visible
 
+```html
+<img showhide='myId3' showhide_visible='true' src='https://www.google.com/images/srpr/logo11w.png' />
+<div id='myId3'>Content goes here</div>
 ```
-<img showhide='myId2' src="https://www.google.com/images/srpr/logo11w.png" />
-<div id="myId2">Content goes here</div>
+
+Example with div and content initially visible
+
+```html
+<a showhide='myId4' showhide_visible='true' href="#">
+  ["When visible", "When hidden"]
+</a>
+<div id="myId4">Content goes here</div>
 ```
--- showhide.js
+++ showhide.js
@@ -1,32 +1,43 @@
 (function($){ 
   $(document).ready(function() {
     var elems = $('*[showhide]'),
-      $elem, id, text, settings;
+      $elem, id, text, settings, visible;
 
     for (var i = 0; i < elems.length; i++) {
       $elem = elems.eq(i);
       id = "#" + $elem.attr("showhide");
+      visible = !!$elem.attr("showhide_visible");
       text = $elem.text();
 
+      // check if the element has text inside it and attempt to parse it
       if (text){
-        settings = JSON.parse($elem.text());
-        $elem.data("wpsh_show", settings[0]);
-        $elem.data("wpsh_hide", settings[1]);
-
-        if (settings.length > 2 && settings[2]) {
-          $elem.text(settings[1]);
-          $(id).hide();
-        }
-        else {
-          $elem.text(settings[0]);
-          $(id).show();
+        try {
+          settings = JSON.parse($elem.text());
+          $elem.data("wpsh_show", settings[0]);
+          $elem.data("wpsh_hide", settings[1]);
+        }catch(e){
+          console.error("Error parsing text inside showhide element", $elem);
+          console.error(e);
+          continue;
         }
       }
 
       $elem.data("wpsh_id", id);
 
+      if (visible) {
+        if ($elem.data("wpsh_show")){
+          $elem.text($elem.data("wpsh_show"));
+        }
+        $(id).show();
+      }
+      else {
+        if ($elem.data("wpsh_hide")){
+          $elem.text($elem.data("wpsh_hide"));
+        }
+        $(id).hide();
+      }
+
       $elem.click(function(e) {
-        console.log(e);
         var $elem = $(e.currentTarget);
         var id = $elem.data("wpsh_id");
         if ($(id).is(":visible")) {
